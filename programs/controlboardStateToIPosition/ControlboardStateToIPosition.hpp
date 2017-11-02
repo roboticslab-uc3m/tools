5209e3df3c8831a81348cd654585368abcf6d6c3
jgvictores <jcgvicto@gmail.com>
2017-11-02 12:10:57 +0100

    begin ControlboardStateToIPosition class

---
+++ programs/controlboardStateToIPosition/ControlboardStateToIPosition.hpp
@@ -0,0 +1,12 @@
+
+#ifndef __CONTROL_STATE_TO_I_POSITION_HPP__
+#define __CONTROL_STATE_TO_I_POSITION_HPP__
+
+
+class ControlboardStateToIPosition
+{
+public:
+    ControlboardStateToIPosition();
+};
+
+#endif // __CONTROL_STATE_TO_I_POSITION_HPP__
