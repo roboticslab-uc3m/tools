77437c968259cf667ae642307c6a1d20fb94c6d1
jgvictores <jcgvicto@gmail.com>
2019-11-20 19:35:39 +0100

    opens remote_controlboards

--- libraries/YarpPlugins/RealToSimControlboard/DeviceDriverImpl.cpp
+++ libraries/YarpPlugins/RealToSimControlboard/DeviceDriverImpl.cpp
@@ -11,22 +11,43 @@ bool RealToSimControlboard::open(yarp::os::Searchable& config)
 {
     CD_DEBUG("config: %s\n",config.toString().c_str());
 
-    yarp::dev::PolyDriver* remoteControlboard = new yarp::dev::PolyDriver;
-
-    yarp::os::Property options;
-    options.put("device","remote_controlboard");
-    options.put("local","/realToSimControlboard/teoSim/leftLacqueyFetch/externalFinger");
-    options.put("remote","/teoSim/leftLacqueyFetch/externalFinger");
-    remoteControlboard->open(options);
-
-    if( ! remoteControlboard->isValid() )
+    if(!config.check("remotes", "list of remotes to which this device connects"))
     {
-        CD_ERROR("yarp plugin not valid.\n");
+        CD_ERROR("Required \"remotes\" list\n");
         return false;
     }
-    CD_SUCCESS("Valid yarp plugin.\n");
+    yarp::os::Bottle* remotesList = config.find("remotes").asList();
+    if (!remotesList)
+    {
+        CD_ERROR("Error parsing parameters: \"remotes\" should be followed by a list\n");
+        return false;
+    }
+    CD_DEBUG("%s\n", remotesList->toString().c_str());
 
-    remoteControlboards.push_back(remoteControlboard);
+    for(size_t i=0; i< remotesList->size(); i++)
+    {
+        std::string remoteGroupName = remotesList->get(i).asString();
+        if(!config.check(remoteGroupName))
+        {
+            CD_ERROR("\"%s\" group not found!\n", remoteGroupName.c_str());
+            return false;
+        }
+        CD_SUCCESS("\"%s\" group found!\n", remoteGroupName.c_str());
+        yarp::os::Bottle remoteGroup = config.findGroup(remoteGroupName);
+        CD_INFO("%s\n", remoteGroup.toString().c_str());
+        yarp::os::Property options;
+        options.fromString(remoteGroup.toString());
+
+        yarp::dev::PolyDriver* remoteControlboard = new yarp::dev::PolyDriver(options);
+        if( ! remoteControlboard->isValid() )
+        {
+            CD_ERROR("yarp plugin not valid.\n");
+            return false;
+        }
+        CD_SUCCESS("Valid yarp plugin.\n");
+
+        remoteControlboards.push_back(remoteControlboard);
+    }
 
     return true;
 }
