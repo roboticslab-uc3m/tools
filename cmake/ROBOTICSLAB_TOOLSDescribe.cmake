5d030ceefe1cb9d0faac39158b111be8c46b6e95
Bartek Łukawski <bwmn.peter@gmail.com>
2019-12-27 00:12:47 +0100

    Update CMake/YCM export options, upgrade CMake and YARP
    https://github.com/roboticslab-uc3m/questions-and-answers/issues/84

--- cmake/ROBOTICSLAB_TOOLSDescribe.cmake
+++ cmake/ROBOTICSLAB_TOOLSDescribe.cmake
@@ -1,5 +1,5 @@
 # Store the package in the user registry.
-export(PACKAGE ROBOTICSLAB_TOOLS)
+set(CMAKE_EXPORT_PACKAGE_REGISTRY ON)
 
 # Create and install config and version files (YCM).
 include(InstallBasicPackageFiles)
