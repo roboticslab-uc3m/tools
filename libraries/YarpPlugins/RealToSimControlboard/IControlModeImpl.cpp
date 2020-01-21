55243d8cccb1a3c21f019ec8d4c9738027ec3774
jgvictores <jcgvicto@gmail.com>
2020-01-21 19:47:46 +0100

    init add IControlModeImpl

---
+++ libraries/YarpPlugins/RealToSimControlboard/IControlModeImpl.cpp
@@ -0,0 +1,47 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "RealToSimControlboard.hpp"
+
+// ------------------ IControlMode Related ----------------------------------------
+
+bool getControlMode(int j, int * mode)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool getControlModes(int * modes)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool getControlModes(int n_joint, const int * joints, int * modes)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool setControlMode(int j, const int mode)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool setControlModes(int n_joint, const int * joints, int * modes)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool setControlModes(int * modes)
+{
+    return true;
+}
+
+// -----------------------------------------------------------------------------
