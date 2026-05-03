3f7d8fd279161abe8ef962e8ec7ca3ccc520a1ca
Bartek Łukawski <bwmn.peter@gmail.com>
2026-05-03 14:38:51 +0200

    Improve base parsing of config file

--- libraries/YarpPlugins/BasicJointCoupling/DeviceDriverImpl.cpp
+++ libraries/YarpPlugins/BasicJointCoupling/DeviceDriverImpl.cpp
@@ -39,19 +39,52 @@ bool BasicJointCoupling::open(yarp::os::Searchable & config)
         return false;
     }
 
-    const auto actuatedAxes = fullConfig.findGroup("actuatedAxes").tail();
-    const auto physicalJoints = fullConfig.findGroup("physicalJoints").tail();
+    const auto * actuatedAxes = fullConfig.find("actuatedAxes").asList();
+    const auto * physicalJoints = fullConfig.find("physicalJoints").asList();
+    const auto * physicalJointLimitsMins = fullConfig.find("mins").asList();
+    const auto * physicalJointLimitsMaxs = fullConfig.find("maxs").asList();
 
-    if (actuatedAxes.isNull() || actuatedAxes.size() == 0)
+    if (!actuatedAxes || actuatedAxes->size() == 0)
     {
-        yCError(BJC) << "Missing or empty \"actuatedAxes\" section collection";
+        yCError(BJC) << "Missing or empty 'actuatedAxes' property or not a list";
         return false;
     }
 
-    if (physicalJoints.isNull() || physicalJoints.size() == 0)
+    if (!physicalJoints || physicalJoints->size() == 0)
     {
-        yCError(BJC) << "Missing or empty \"physicalJoints\" section collection";
+        yCError(BJC) << "Missing or empty 'physicalJoints' property or not a list";
         return false;
+    }
+
+    if (!physicalJointLimitsMins || physicalJointLimitsMins->size() != physicalJoints->size())
+    {
+        yCError(BJC) << "Missing or empty 'mins' property, not a list, or its size does not match the number of physical joints";
+        return false;
+    }
+
+    if (!physicalJointLimitsMaxs || physicalJointLimitsMaxs->size() != physicalJoints->size())
+    {
+        yCError(BJC) << "Missing or empty 'maxs' property, not a list, or its size does not match the number of physical joints";
+        return false;
+    }
+
+    yCInfo(BJC) << "Actuated axes:" << actuatedAxes->toString();
+    yCInfo(BJC) << "Physical joints:" << physicalJoints->toString();
+    yCInfo(BJC) << "Physical joint limits mins:" << physicalJointLimitsMins->toString();
+    yCInfo(BJC) << "Physical joint limits maxs:" << physicalJointLimitsMaxs->toString();
+
+    for (auto i = 0; i < actuatedAxes->size(); i++)
+    {
+        const auto actuatedAxisName = actuatedAxes->get(i).asString();
+        const auto group = fullConfig.findGroup(actuatedAxisName).tail();
+
+        if (group.isNull() || group.size() == 0)
+        {
+            yCError(BJC) << "Missing or empty section for actuated axis" << actuatedAxisName;
+            return false;
+        }
+
+        yCInfo(BJC) << "Actuated axis" << actuatedAxisName << "is coupled with:" << group.toString();
     }
 
     return true;
